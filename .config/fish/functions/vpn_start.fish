1170661e2944e2fc132f1eb48ec4c38791bd5909
alexandre.alessi <alexandre.alessi@gmail.com>
2023-01-02 19:09:03 -0300

    config

--- .config/fish/functions/vpn_start.fish
+++ .config/fish/functions/vpn_start.fish
@@ -2,7 +2,8 @@ function vpn_start
     set PARAM_GTW (cat /data/params/vpn_portal)
     set PARAM_USER (cat /data/params/vpn_email)
     set PARAM_AUTH_GROUP (cat /data/params/vpn_auth-group)
-    set PARAM_CRT (cat /data/params/vpn_crt)
+    # set PARAM_CRT (cat /data/params/vpn_crt)
+    set PARAM_CRT (p11tool --provider=/usr/lib/libeToken.so --list-all-certs --only-urls | tail -1)
     set PARAM_SERVER_CERT (cat /data/params/vpn_server-cert)
     
     sudo openconnect --authgroup=$PARAM_AUTH_GROUP \
