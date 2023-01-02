6a9c5effb5d10f11e55fabd634d1a9de626b6ea8
alexandre.alessi <alexandre.alessi@gmail.com>
2023-01-02 13:23:46 -0300

    fish functions

---
+++ .config/fish/functions/vpn_start.fish
@@ -0,0 +1,10 @@
+function vpn_start
+    set PARAM_GTW (cat /data/params/vpn_portal)
+    set PARAM_USER (cat /data/params/vpn_email)
+    set PARAM_AUTH_GROUP (cat /data/params/vpn_auth-group)
+    set PARAM_CRT (cat /data/params/vpn_crt)
+    set PARAM_SERVER_CERT (cat /data/params/vpn_server-cert)
+    
+    sudo openconnect --authgroup=$PARAM_AUTH_GROUP \
+        --protocol=gp -u $PARAM_USER -c $PARAM_CRT -v $PARAM_GTW 
+end
